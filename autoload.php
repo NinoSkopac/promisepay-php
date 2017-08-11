7417d478d756333c0cdf0bcb9fd548ae2e06c6f7
ninoskopac <nino.skopac@gmail.com>
2017-08-11 22:55:36 +0200

    #60 Changes to how devel autoloading is performed

--- autoload.php
+++ autoload.php
@@ -1,28 +1,30 @@
 <?php
 namespace PromisePay;
 
+// This file exists for people who haven't installed using Composer
+
 spl_autoload_register(function ($class) {
     $namespaceLength = strlen(__NAMESPACE__);
-    
+
     if (substr($class, 0, $namespaceLength) != __NAMESPACE__) {
         // doesn't belong to our package
         return;
     }
-    
+
     $classPath = substr($class, $namespaceLength);
-    
+
     $file = __DIR__ . '/lib/' . trim(str_replace('\\', '/', $classPath), '/') . '.php';
-    
+
     if (file_exists($file)) {
         require $file;
     } else {
         printf("%s (path: %s) wasn't found." . PHP_EOL, $class, $file);
-        
+
         printf(
             'Debug backtrace: %s' . PHP_EOL,
             print_r(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), true)
         );
-        
+
         die();
     }
 });
@@ -39,7 +41,7 @@ if (!class_exists('Httpful\Request')) {
     foreach ($httpfulFiles as $file) {
         require_once $file;
     }
-
-    // polyfill for array_column
-    require_once __DIR__ . '/lib/Vendors/ramsey/array_column/src/array_column.php';
 }
+
+// polyfill for array_column
+require_once __DIR__ . '/lib/Vendors/ramsey/array_column/src/array_column.php';
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,29 +1,20 @@
 <?php
 namespace PromisePay\Tests;
 
-use Composer\Autoload\ClassLoader;
 use PromisePay\PromisePay;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-$composerAutoloadFile = __DIR__ . '/../../../autoload.php';
-
-if (is_file($composerAutoloadFile)) {
-    require_once $composerAutoloadFile;
-}
+// AUTOLOADERS
+// PromisePay autoloader
+require __DIR__ . '/../autoload.php';
+// Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
 
 // Tests/PHPunit specific files
 require_once __DIR__ . '/functions.php';
-
-// test assets autoloader
-$classLoader = new ClassLoader();
-$classLoader->addPsr4(__NAMESPACE__ . '\\', __DIR__, true);
-$classLoader->register();
-
-// Project files autoloader
-require __DIR__ . '/../autoload.php';
 
 // PHPUnit 6 introduced a breaking change that
 // removes PHPUnit_Framework_TestCase as a base class,
@@ -35,4 +26,3 @@ if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Frame
 PromisePay::Configuration()->environment('prelive');
 PromisePay::Configuration()->login('idsidorov@gmail.com');
 PromisePay::Configuration()->password('d897f812e8485728e1de7d8ae092b75a');
-
